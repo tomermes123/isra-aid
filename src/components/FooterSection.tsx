a1cb5f4da46c4e4ece21951e4433c65b7adc94bd
Guy Messika <guy032@gmail.com>
2023-10-10 17:05:51 +0300

    * style(FooterSection.tsx): adjust height and padding of Footer component * style(FooterSection.tsx): adjust font-size and padding of StyledText component * style(FooterSection.tsx): adjust font-size and padding of StyledTextSmall component * style(FooterSection.tsx): adjust width and height of SocialIcon component * refactor(FooterSection.tsx): remove Background and Gradient components * refactor(FooterSection.tsx): add alt text to logo and SocialIcon components * refactor(FooterSection.tsx): remove unused import

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -9,42 +9,34 @@ const Footer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 798px;
+  height: auto;
+  padding: 100px 20px;
   width: 100%;
   margin-top: 20px;
-`
-
-const Background = styled.img`
-  position: absolute;
-  top: 0;
-  height: 100%;
-  width: 100%;
-  object-fit: cover;
-  background: linear-gradient(rgba(0, 0, 0, 0.46), rgba(0, 0, 0, 0));
-`
-
-const Gradient = styled.div`
-  position: absolute;
-  top: 0;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0.66), rgba(0, 0, 0, 0));
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backgroundFooter}) no-repeat center center/cover;
 `
 
 const FooterContent = styled.div`
   z-index: 1;
+  text-align: center;
 `
 
 const StyledText = styled.div`
   font-family: "Poppins", sans-serif;
   font-weight: 700;
   font-size: 56px;
-  line-height: 73.92px;
-  text-align: center;
+  line-height: 1.3;
   color: white;
   text-transform: uppercase;
+  margin-bottom: 20px;
+  padding: 10px 0; // Added padding
+
   @media (max-width: 768px) {
-    font-size: 48px;
+    font-size: 36px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 28px;
   }
 `
 
@@ -52,25 +44,31 @@ const StyledTextSmall = styled.div`
   font-family: "Poppins", sans-serif;
   font-weight: 400;
   font-size: 20px;
-  line-height: 73.92px;
-  text-align: center;
+  line-height: 1.3;
   color: white;
+  margin-bottom: 10px;
+  padding: 10px 0; // Added padding
+
   @media (max-width: 768px) {
+    font-size: 18px;
+  }
+
+  @media (max-width: 480px) {
     font-size: 16px;
   }
 `
 
 const SocialIcon = styled.img`
   cursor: pointer;
+  width: 40px; // Adjust the size as needed
+  height: auto;
 `
 
 export function FooterSection () {
   return (
     <Footer id='contact-us'>
-      <Background src={backgroundFooter} alt='background' />
-      <Gradient />
       <FooterContent>
-        <img src={logo} />
+        <img src={logo} alt='IsraAid Logo' style={{ maxWidth: '100%', height: 'auto' }} />
         <StyledText>
           Let’s make Israel
           <br /> stronger together
@@ -78,11 +76,8 @@ export function FooterSection () {
         <StyledTextSmall>israaid71023@gmail.com</StyledTextSmall>
         <SocialIcon
           src={facebookLogo}
-          onClick={(e: any) =>
-            window.open(
-              'https://www.facebook.com/groups/308278374859148',
-              '_blank',
-            )}
+          alt='Facebook Logo'
+          onClick={() => window.open('https://www.facebook.com/groups/308278374859148', '_blank')}
         />
       </FooterContent>
     </Footer>
