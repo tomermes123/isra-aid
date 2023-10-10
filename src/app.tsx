3544bde7c0815faafff3439f1904d84887df9abc
Tomer <tomermes123@gmail.com>
2023-10-10 07:41:16 +0300

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,57 +1,57 @@
-import { useState } from "react";
-import { ReactComponent as Logo } from "./logo.svg";
+import { useState } from 'react'
+import { ReactComponent as Logo } from './logo.svg'
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Link,
   useParams,
-} from "react-router-dom";
-import { LinkDetail } from "./components/aid-link";
-import "./app.css";
+} from 'react-router-dom'
+import { LinkDetail } from './components/aid-link'
+import './app.css'
 
 const Home: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo className="App-logo" title="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <Logo className='App-logo' title='logo' />
         <p>Hello IsraAid123!</p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
-          {" | "}
+          {' | '}
           <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Vite Docs
           </a>
         </p>
       </header>
     </div>
-  );
-};
+  )
+}
 
-export function App() {
-  const [count, setCount] = useState(0);
+export function App () {
+  const [count, setCount] = useState(0)
 
   return (
     <Router>
       <Routes>
-        <Route path="/links/:linkId" element={<LinkDetail />} />
-        <Route path="/" element={<Home />} />
+        <Route path='/links/:linkId' element={<LinkDetail />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </Router>
-  );
+  )
 }
--- src/components/aid-link.tsx
+++ src/components/aid-link.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom'
 
 export type AidLink = {
   id: string;
@@ -7,84 +7,84 @@ export type AidLink = {
   content: string;
 };
 
-const sheetId = "1G5E8gdgYnc3RT8jyfNhU_J2a2paEdXt5RH7rCmFJPJo";
+const sheetId = '1G5E8gdgYnc3RT8jyfNhU_J2a2paEdXt5RH7rCmFJPJo'
 
 export const links: AidLink[] = [
   {
-    id: "ghi4",
-    category: "a",
-    name: "fourth-link",
-    content: "This is the fourth link.",
+    id: 'ghi4',
+    category: 'a',
+    name: 'fourth-link',
+    content: 'This is the fourth link.',
   },
   {
-    id: "jkl5",
-    category: "a",
-    name: "fifth-link",
-    content: "This is the fifth link.",
+    id: 'jkl5',
+    category: 'a',
+    name: 'fifth-link',
+    content: 'This is the fifth link.',
   },
   {
-    id: "mno6",
-    category: "a",
-    name: "sixth-link",
-    content: "This is the sixth link.",
+    id: 'mno6',
+    category: 'a',
+    name: 'sixth-link',
+    content: 'This is the sixth link.',
   },
   {
-    id: "pqr7",
-    category: "a",
-    name: "seventh-link",
-    content: "This is the seventh link.",
+    id: 'pqr7',
+    category: 'a',
+    name: 'seventh-link',
+    content: 'This is the seventh link.',
   },
   {
-    id: "stu8",
-    category: "a",
-    name: "eighth-link",
-    content: "This is the eighth link.",
+    id: 'stu8',
+    category: 'a',
+    name: 'eighth-link',
+    content: 'This is the eighth link.',
   },
   {
-    id: "vwx9",
-    category: "b",
-    name: "ninth-link",
-    content: "This is the ninth link.",
+    id: 'vwx9',
+    category: 'b',
+    name: 'ninth-link',
+    content: 'This is the ninth link.',
   },
   {
-    id: "yza10",
-    category: "b",
-    name: "tenth-link",
-    content: "This is the tenth link.",
+    id: 'yza10',
+    category: 'b',
+    name: 'tenth-link',
+    content: 'This is the tenth link.',
   },
   {
-    id: "bcd11",
-    category: "b",
-    name: "eleventh-link",
-    content: "This is the eleventh link.",
+    id: 'bcd11',
+    category: 'b',
+    name: 'eleventh-link',
+    content: 'This is the eleventh link.',
   },
   {
-    id: "efg12",
-    category: "b",
-    name: "twelfth-link",
-    content: "This is the twelfth link.",
+    id: 'efg12',
+    category: 'b',
+    name: 'twelfth-link',
+    content: 'This is the twelfth link.',
   },
   {
-    id: "hij13",
-    category: "b",
-    name: "thirteenth-link",
-    content: "This is the thirteenth link.",
+    id: 'hij13',
+    category: 'b',
+    name: 'thirteenth-link',
+    content: 'This is the thirteenth link.',
   },
 
   // ... add more link as needed
-];
+]
 
 export const LinkDetail: React.FC = () => {
-  const { linkId } = useParams<{ linkId: string }>();
+  const { linkId } = useParams<{ linkId: string }>()
 
-  const link: AidLink | undefined = links.find((l) => l.id === linkId);
+  const link: AidLink | undefined = links.find((l) => l.id === linkId)
 
-  if (!link) return <div>Link not found!</div>;
+  if (!link) return <div>Link not found!</div>
 
   return (
     <div>
       <h2>{link.name}</h2>
       <p>{link.content}</p>
     </div>
-  );
-};
+  )
+}
